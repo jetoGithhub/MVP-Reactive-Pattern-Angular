eebc35a146415d1a5bc3d19f74b729f064b83326
jetoGithhub <jetox21@gmail.com>
2023-06-07 18:28:38 -0500

    1 file modified

--- src/app/models/user/user.model.ts
+++ src/app/models/user/user.model.ts
@@ -5,18 +5,16 @@ export class UserModel implements User {
   readonly name: string;
   readonly nickname: string;
   readonly email: string;
-  readonly addres: string;
   readonly avatar =
     'https://avatar-management--avatars.us-west-2.prod.public.atl-paas.net/default-avatar.png';
 
-  constructor(response: ApiUser.BodyResponse) {
+  get addres() {
+    return `${this.response.address?.street}, ${this.response.address?.city} - ${this.response.address?.zipcode}`;
+  }
+
+  constructor(private response: ApiUser.BodyResponse) {
     this.name = response?.name ?? '';
     this.nickname = response?.username ?? '';
     this.email = response?.email?.toLowerCase() ?? '';
-    this.addres = this.setAddres(response.address);
-  }
-
-  private setAddres(addres: ApiUser.Address) {
-    return `${addres?.street}, ${addres?.city} - ${addres?.zipcode}`;
   }
 }
